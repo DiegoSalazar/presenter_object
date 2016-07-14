e63cc5b50e038221aa6812c364a3e1458ce7e951
Diego Salazar <diego@greyrobot.com>
2016-07-14 14:37:15 -0400

    added spec to presentable

--- spec/units/presentable_spec.rb
+++ spec/units/presentable_spec.rb
@@ -8,11 +8,17 @@ class User
 end
 
 describe PresenterObject::Presentable do
-  context "#presenterize" do
-    let(:user) { User.new }
+  let(:user) { User.new }
 
+  context "#presenterize" do
     it "returns the object wrapped in its presenter" do
-      expect(user.presenterize).to be_a UserPresenter
+      expect(user.presenterize).to be_kind_of UserPresenter
+    end
+  end
+
+  context ".presenter_class" do
+    it "is the presenter class" do
+      expect(User.presenter_class).to be UserPresenter
     end
   end
 end
